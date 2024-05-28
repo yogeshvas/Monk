7fe2145bb73b3c03174608d9e4801a70e55307cb
Yogesh Vashisth <writetokhair@gmail.com>
2024-05-28 18:07:29 +0530

    Swipe left to open todo screen added

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,14 +1,31 @@
-import React, {useState} from 'react';
-import {StyleSheet, View, SafeAreaView, Text} from 'react-native';
+import React, {useRef} from 'react';
+import {StyleSheet, View, SafeAreaView, PanResponder} from 'react-native';
 import Header from '../components/Header';
 import Body from '../components/Body';
 import Footer from '../components/Footer';
-import {InstalledApps, RNLauncherKitHelper} from 'react-native-launcher-kit';
-
 
 const Home = ({navigation}: any) => {
+  const panResponder = useRef(
+    PanResponder.create({
+      onMoveShouldSetPanResponder: (evt, gestureState) => {
+        // Check if the swipe is left
+        return gestureState.dx < -20;
+      },
+      onPanResponderRelease: (evt, gestureState) => {
+        if (gestureState.dx < -100) {
+          // If the swipe distance is enough, navigate to the Todo screen
+          navigation.navigate('todo');
+        }
+      },
+    }),
+  ).current;
+
   return (
     <SafeAreaView style={styles.container}>
+      <View
+        style={styles.swipeZone}
+        {...panResponder.panHandlers} // Attach the PanResponder to this view
+      />
       <View style={styles.headerContainer}>
         <Header />
       </View>
@@ -28,6 +45,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#101115',
+  },
+  swipeZone: {
+    position: 'absolute',
+    right: 0,
+    top: 0,
+    bottom: 0,
+    width: 30, // Adjust the width as needed to make it sensitive at the corner
+    zIndex: 1,
   },
   headerContainer: {
     flex: 1,
